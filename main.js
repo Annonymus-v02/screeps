b2149202c75c666440394e01bc73aef9044ef817
Annonymuys <terehm@usi.com>
2019-10-28 10:55:39 +0100

    add debug log

--- main.js
+++ main.js
@@ -167,18 +167,22 @@ module.exports.loop = function () {
 
     for (let name in Game.creeps) {
         let creep = Game.creeps[name];
-        switch (creep.memory.role) {
-            case 'harvester':
-                roleHarvester.run(creep);
-                break;
-            case 'upgrader':
-                roleUpgrader.run(creep);
-                break;
-            case 'builder':
-                roleBuilder.run(creep);
-                break;
-            default:
-                console.log('Unknown role:', creep.memory.role);
+        try {
+            switch (creep.memory.role) {
+                case 'harvester':
+                    roleHarvester.run(creep);
+                    break;
+                case 'upgrader':
+                    roleUpgrader.run(creep);
+                    break;
+                case 'builder':
+                    roleBuilder.run(creep);
+                    break;
+                default:
+                    console.log('Unknown role:', creep.memory.role);
+            }
+        }catch(err) {
+            console.log(name, creep.memory);
         }
     }
 
