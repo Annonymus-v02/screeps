f4dea878522d6c4c56daf35ed4520c64dd969b3e
Annonymuys <terehm@usi.com>
2019-10-28 10:57:06 +0100

    don't try to process unborn creeps

--- main.js
+++ main.js
@@ -167,22 +167,19 @@ module.exports.loop = function () {
 
     for (let name in Game.creeps) {
         let creep = Game.creeps[name];
-        try {
-            switch (creep.memory.role) {
-                case 'harvester':
-                    roleHarvester.run(creep);
-                    break;
-                case 'upgrader':
-                    roleUpgrader.run(creep);
-                    break;
-                case 'builder':
-                    roleBuilder.run(creep);
-                    break;
-                default:
-                    console.log('Unknown role:', creep.memory.role);
-            }
-        }catch(err) {
-            console.log(name, creep.memory);
+        if (creep.spawning) continue;
+        switch (creep.memory.role) {
+            case 'harvester':
+                roleHarvester.run(creep);
+                break;
+            case 'upgrader':
+                roleUpgrader.run(creep);
+                break;
+            case 'builder':
+                roleBuilder.run(creep);
+                break;
+            default:
+                console.log('Unknown role:', creep.memory.role);
         }
     }
 
