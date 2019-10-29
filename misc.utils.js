f2259efcb1691b106bafce4d130ca8f96f96194c
Annonymuys <terehm@usi.com>
2019-10-29 10:18:16 +0100

    fix bug

--- misc.utils.js
+++ misc.utils.js
@@ -29,7 +29,7 @@ module.exports = {
 
     mineManual: function(creep) {
         // TODO: make better
-        mine(creep);
+        this.mine(creep);
     },
     /** @param {Creep} creep **/
     upgradeController: function(creep) {
