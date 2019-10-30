65facd0b0dd2e464a8b35512d8e77b92cd0a3c59
Annonymuys <terehm@usi.com>
2019-10-30 11:42:09 +0100

    fix err  log

--- main.js
+++ main.js
@@ -147,7 +147,8 @@ module.exports.loop = function () {
         for(let name in Memory.creeps) {
             if(!Game.creeps[name]) {
                 if (Memory.creeps[name].source) {
-                    utils.err(Memory.creeps[name].spawn, Game.spawns[Memory.creeps[name].spawn].room.memory.sources);
+                    utils.err(Memory.creeps[name].spawn);
+                    utils.err(Game.spawns[Memory.creeps[name].spawn].room.memory.sources);
                     --Game.spawns[Memory.creeps[name].spawn].room.memory.sources[Memory.creeps[name].source];
                 }
                 delete Memory.creeps[name];
